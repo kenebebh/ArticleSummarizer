4cff387ec30d227701431a1fce1295f284350900
kenebebh <kenebebhbanigo@gmail.com>
2023-04-26 14:24:57 +0100

    added all initial files, and removed unused files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Demo.jsx
@@ -0,0 +1,5 @@
+const Demo = () => {
+  return <div>DEMO</div>;
+};
+
+export default Demo;
