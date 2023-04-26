9ada328b4c6f9a9e7d0caed98bf00815e7d194ef
kenebebh <kenebebhbanigo@gmail.com>
2023-04-26 18:06:38 +0100

    added basic structure of the webapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./index.css";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
 function App() {
   return (
     <main>
       <div className="main">
         <div className="gradient"></div>
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
       </div>
     </main>
   );
